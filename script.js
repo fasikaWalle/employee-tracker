f7f89d99ea9056477ebd9df4e006adc2a98a6fb2
fasikaWalle <fasikabini12@gmail.com>
2021-03-12 21:49:46 -0600

    add user choice

--- script.js
+++ script.js
@@ -10,3 +10,26 @@ const con = mysql.createConnection(
    {host:'localhost', user: 'root',password:'root', database: 'employees'}
  );
  console.log((figlet.textSync('Employee Tracker',{horizontallayout:'full'})).bold);
+ class employees {
+   userChoice(){
+      inquirer.prompt([
+         {
+          type:"list",
+          name:"type",
+          message:"what would you like to do?".yellow,
+          choices:["View all departments","View all roles","View all employees","Add a department","Add a role","Add an employee","Update an employee role","Update employee manager","View employee by manager","View employee by department","Delete tables records","Total budget of a department"]
+         }   
+      ]).then(({type})=>{
+         this.checkChoice(type)
+      }).catch((error) => {
+         throw error;
+       });
+   };
+   
+   
+   
+   
+   
+   
+ }
+ 
